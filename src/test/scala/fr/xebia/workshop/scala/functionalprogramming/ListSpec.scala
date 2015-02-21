e4c55e380355dba33bf56cb073a4cbc6b68e3baa
Clément HELIOU <cheliou@xebia.fr>
2015-02-21 15:37:53 +0100

    feat(list): add starting state

---
+++ src/test/scala/fr/xebia/workshop/scala/functionalprogramming/ListSpec.scala
@@ -0,0 +1,16 @@
+package fr.xebia.workshop.scala.functionalprogramming
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class ListSpec extends FlatSpec with Matchers{
+
+  // Exercise 1
+
+//  "A List" should "provide a single-argument constructor" in {
+//    Cons(1)
+//  }
+//
+//  it should "provide a bi-argument constructor" in {
+//    Cons(2, Nil)
+//  }
+}
