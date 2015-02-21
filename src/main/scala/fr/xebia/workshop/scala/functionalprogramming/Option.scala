9130eb268a8825d9c793da88cd2ba2db9cb182ad
Clément HELIOU <cheliou@xebia.fr>
2015-02-21 16:37:39 +0100

    feat(option): add getOrElse method
    Related to exercise-202.

--- src/main/scala/fr/xebia/workshop/scala/functionalprogramming/Option.scala
+++ src/main/scala/fr/xebia/workshop/scala/functionalprogramming/Option.scala
@@ -1,6 +1,11 @@
 package fr.xebia.workshop.scala.functionalprogramming
 
-sealed trait Option[A] {}
+sealed trait Option[A] {
+  def getOrElse[B >: A](default: B): B = this match {
+    case Some(value) => value
+    case _ => default
+  }
+}
 
 case object None extends Option[Nothing]
 
--- src/test/scala/fr/xebia/workshop/scala/functionalprogramming/OptionSpec.scala
+++ src/test/scala/fr/xebia/workshop/scala/functionalprogramming/OptionSpec.scala
@@ -12,26 +12,49 @@ class OptionSpec extends FlatSpec with Matchers{
     Some(7)
   }
 
-//  "An empty Option" should "return the default value when getOrElse is called" in {
+  "An empty Option" should "return the default value when getOrElse is called" in {
+    // Arrange
+    val emptyOption = None
+
+    // Act
+    val result = emptyOption.getOrElse(2)
+
+    // Assert
+    result shouldEqual 2
+  }
+
+//  it should "return the alternative evaluation when orElse is called" in {
 //    // Arrange
+//    val anAlternative = Some(6)
 //    val emptyOption = None
 //
 //    // Act
-//    val result = emptyOption.getOrElse(2)
+//    val result = emptyOption.orElse(anAlternative)
 //
 //    // Assert
-//    result shouldEqual 2
+//    result shouldEqual Some(6)
 //  }
 
-//  "A non-empty Option" should "return its value when getOrElse is called" in {
+  "A non-empty Option" should "return its value when getOrElse is called" in {
+    // Arrange
+    val anOption = Some(6)
+
+    // Act
+    val result = anOption.getOrElse(2)
+
+    // Assert
+    result shouldEqual 6
+  }
+
+//  it should "return its value when orElse is called" in {
 //    // Arrange
 //    val anOption = Some(6)
 //
 //    // Act
-//    val result = anOption.getOrElse(2)
+//    val result = anOption.orElse(Some(4))
 //
 //    // Assert
-//    result shouldEqual 6
+//    result shouldEqual anOption
 //  }
 
 }
