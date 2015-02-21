fa26827ebe4ae289c9711937e85b6c75f6114168
Clément HELIOU <cheliou@xebia.fr>
2015-02-21 16:39:47 +0100

    feat(option): add orElse method
    Related to exercise-203.

--- src/main/scala/fr/xebia/workshop/scala/functionalprogramming/Option.scala
+++ src/main/scala/fr/xebia/workshop/scala/functionalprogramming/Option.scala
@@ -1,9 +1,14 @@
 package fr.xebia.workshop.scala.functionalprogramming
 
-sealed trait Option[A] {
+sealed trait Option[+A] {
   def getOrElse[B >: A](default: B): B = this match {
     case Some(value) => value
     case _ => default
+  }
+
+  def orElse[B >: A](alternative: Option[B]): Option[B] = this match {
+    case Some(_) => this
+    case _ => alternative
   }
 }
 
--- src/test/scala/fr/xebia/workshop/scala/functionalprogramming/OptionSpec.scala
+++ src/test/scala/fr/xebia/workshop/scala/functionalprogramming/OptionSpec.scala
@@ -23,16 +23,27 @@ class OptionSpec extends FlatSpec with Matchers{
     result shouldEqual 2
   }
 
-//  it should "return the alternative evaluation when orElse is called" in {
+  it should "return the alternative evaluation when orElse is called" in {
+    // Arrange
+    val anAlternative = Some(6)
+    val emptyOption = None
+
+    // Act
+    val result = emptyOption.orElse(anAlternative)
+
+    // Assert
+    result shouldEqual Some(6)
+  }
+
+//  it should "return None when filter is called" in {
 //    // Arrange
-//    val anAlternative = Some(6)
-//    val emptyOption = None
+//    val emptyOption: Option[Int] = None
 //
 //    // Act
-//    val result = emptyOption.orElse(anAlternative)
+//    val result = emptyOption.filter(_ % 2 == 0)
 //
 //    // Assert
-//    result shouldEqual Some(6)
+//    result shouldEqual None
 //  }
 
   "A non-empty Option" should "return its value when getOrElse is called" in {
@@ -46,15 +57,36 @@ class OptionSpec extends FlatSpec with Matchers{
     result shouldEqual 6
   }
 
-//  it should "return its value when orElse is called" in {
+  it should "return its value when orElse is called" in {
+    // Arrange
+    val anOption = Some(6)
+
+    // Act
+    val result = anOption.orElse(Some(4))
+
+    // Assert
+    result shouldEqual anOption
+  }
+//
+//  it should "return None when filter with a predicate that does not match it" in {
+//    // Arrange
+//    val anOption = Some(7)
+//
+//    // Act
+//    val result = anOption.filter(_ % 2 == 0)
+//
+//    // Assert
+//    result shouldEqual None
+//  }
+//
+//  it should "return its value when filtered with a predicate matching it" in {
 //    // Arrange
 //    val anOption = Some(6)
 //
 //    // Act
-//    val result = anOption.orElse(Some(4))
+//    val result = anOption.filter(_ % 2 == 0)
 //
 //    // Assert
 //    result shouldEqual anOption
 //  }
-
 }
