76271a2e451859a727731574b48b3df1f5f43926
Clément HELIOU <cheliou@xebia.fr>
2015-02-21 15:51:23 +0100

    feat(list): add tail method
    Related to exercise-102.

--- src/main/scala/fr/xebia/workshop/scala/functionalprogramming/List.scala
+++ src/main/scala/fr/xebia/workshop/scala/functionalprogramming/List.scala
@@ -1,7 +1,12 @@
 package fr.xebia.workshop.scala.functionalprogramming
 
-sealed trait List[+A] {}
+sealed trait List[+A] {
+  def tail: List[A] = this match {
+    case Cons(_, tail) => tail
+    case _ => Nil
+  }
+}
 
 case object Nil extends List[Nothing]
 
-case class Cons[A](head: A, tail: List[A] = Nil) extends List[A]
+case class Cons[A](head: A, initialTail: List[A] = Nil) extends List[A]
--- src/test/scala/fr/xebia/workshop/scala/functionalprogramming/ListSpec.scala
+++ src/test/scala/fr/xebia/workshop/scala/functionalprogramming/ListSpec.scala
@@ -16,36 +16,71 @@ class ListSpec extends FlatSpec with Matchers{
 
   // Exercise 2
 
-//  it should "return an empty list when tail is called on empty list" in {
+  it should "return an empty list when tail is called on empty list" in {
+    // Arrange
+    val emptyList = Nil
+
+    // Act
+    val result = emptyList.tail
+
+    // Assert
+    result shouldEqual Nil
+  }
+
+  it should "return an empty list when tail is called on a single-element list" in {
+    // Arrange
+    val aList = Cons(1)
+
+    // Act
+    val result = aList.tail
+
+    // Assert
+    result shouldEqual Nil
+  }
+
+  it should "return all elements except the first one when tail is called on multi-elements list" in {
+    // Arrange
+    val aList = Cons(2, Cons(4, Cons(3)))
+
+    // Act
+    val result = aList.tail
+
+    // Assert
+    result shouldEqual Cons(4, Cons(3))
+  }
+
+  // Exercise 3
+
+//  it should "return an empty list when drop is called on empty list" in {
 //    // Arrange
 //    val emptyList = Nil
 //
 //    // Act
-//    val result = emptyList.tail
+//    val result = emptyList.drop(10)
 //
 //    // Assert
 //    result shouldEqual Nil
 //  }
 //
-//  it should "return an empty list when tail is called on a single-element list" in {
+//  it should "return an empty list when drop size is greater than the list length" in {
 //    // Arrange
-//    val aList = Cons(1)
+//    val aList = Cons(4, Cons(3, Cons(2)))
 //
 //    // Act
-//    val result = aList.tail
+//    val result = aList.drop(6)
 //
 //    // Assert
 //    result shouldEqual Nil
 //  }
 //
-//  it should "return all elements except the first one when tail is called on multi-elements list" in {
+//  it should "return all elements except the first n ones when drop is called" in {
 //    // Arrange
-//    val aList = Cons(2, Cons(4, Cons(3)))
+//    val aList = Cons(4, Cons(3, Cons(2)))
 //
 //    // Act
-//    val result = aList.tail
+//    val result = aList.drop(2)
 //
 //    // Assert
-//    result shouldEqual Cons(4, Cons(3))
+//    result shouldEqual Cons(2)
 //  }
 }
